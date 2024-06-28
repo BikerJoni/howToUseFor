fd499b7c907e85012f7e2ed7cabe9d4306d583e1
JahongirAbdusamatov <jahongir_7440266@mail.ru>
2024-06-28 18:46:51 +0500

    How to creat .exe file

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+
+//string joniBirthDay = "02/02/1995";
+//DateTime dateTime = DateTime.Parse(joniBirthDay);
+//Console.WriteLine($"Joni was born :  {dateTime}");
+
+//DateTimeOffset dateTimeOffset = DateTimeOffset.Parse(joniBirthDay);
+//Console.WriteLine($"Joni was born {dateTimeOffset}");
+
+
+
+string userInput;
+do
+{
+    Guid newGuid = Guid.NewGuid();
+    string guidString = newGuid.ToString();
+    Clipboard.SetText(guidString);
+    Console.WriteLine("do you want another Guid ? (yes or press any bottom to exit )");
+    userInput = Console.ReadLine();
+
+}
+while (userInput == "yes");
